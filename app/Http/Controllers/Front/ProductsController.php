f42deb7ae36ea010d46c3766bd84fbb507f18b12
shawonrog <shawon9324@gmail.com>
2020-09-11 13:34:35 +0600

    front::laravel 8 upgrade+listing page pagination added

--- app/Http/Controllers/Front/ProductsController.php
+++ app/Http/Controllers/Front/ProductsController.php
@@ -2,22 +2,27 @@
 
 namespace App\Http\Controllers\Front;
 
-use App\Category;
+use Illuminate\Pagination\Paginator;
+
 use App\Http\Controllers\Controller;
-use App\Product;
 use Illuminate\Http\Request;
+use App\Category;
+use App\Product;
 
 class ProductsController extends Controller
 {
     public function listing($url)
     {
+
         $page_name = 'Products';
         $categoryCount = Category::where(['url' => $url, 'status' => 1])->count();
         if ($categoryCount > 0) {
+            Paginator::useBootstrap();
             //echo "Category Exists";die;
             $categoryDetails = Category::catDetails($url);
             // echo "<pre>";print_r($categoryDetails);die;
-            $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->get()->toArray();
+            $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->paginate(3);
+            // $categoryProducts = $categoryProducts->paginate(2);
             return view('front.products.listing')->with(compact('categoryDetails', 'categoryProducts', 'page_name'));
         } else {
             abort(404);
--- resources/views/front/products/listing.blade.php
+++ resources/views/front/products/listing.blade.php
@@ -140,21 +140,12 @@
                         <!-- Shop Page Navigation -->
 
                         <div class="shop_page_nav d-flex flex-row">
-                            <div class="page_prev d-flex flex-column align-items-center justify-content-center"><i
-                                    class="fas fa-chevron-left"></i></div>
                             <ul class="page_nav d-flex flex-row">
-                                <li><a href="#">1</a></li>
-                                <li><a href="#">2</a></li>
-                                <li><a href="#">3</a></li>
-                                <li><a href="#">...</a></li>
-                                <li><a href="#">21</a></li>
+                                {{$categoryProducts->links()}}
+                                    {{-- {{ $categoryProducts->appends(['sort'=>'votes'])->links() }} --}}
                             </ul>
-                            <div class="page_next d-flex flex-column align-items-center justify-content-center"><i
-                                    class="fas fa-chevron-right"></i></div>
                         </div>
-
                     </div>
-
                 </div>
             </div>
         </div>
